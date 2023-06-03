93ba65597371c3edf8cbd3164a8096b9cb2d9a88
Scott Hallock <scott.hallock@gmail.com>
2023-06-02 21:31:04 -0500

    add endpoint for serving bundled files

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 
+const { MONGO_URI } = process.env;
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`The server is running at port ${PORT}`));
+if (!MONGO_URI) throw new Error('Missing MONGO_URI environment variable.');
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('✅ Connected to MongoDB'));
+
+// Parse request body as JSON
+app.use(express.json());
+
+// Serve bundled files in the /dist folder
+app.use('/', express.static(path.join(__dirname, '../dist')));
+app.get('/', (req, res) => res.sendFile(__dirname, '../dist/index.html'));
+
+app.listen(PORT, () => console.log(`🚀 Server is running at port ${PORT}`));
